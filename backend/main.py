0db50e86864ecc9124ad9134e75976159c1830dc
Prince-Leo <a061804050713@163.com>
2025-12-07 04:24:50 +0000

    Update

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,13 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 
 # 允许跨域访问（前端可以直接请求）
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["survey.jakestar.cloud"],  # 生产环境建议改为具体域名
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+    # CORSMiddleware,
+    # allow_origins=["survey.jakestar.cloud"],  # 生产环境建议改为具体域名
+    # allow_credentials=True,
+    # allow_methods=["*"],
+    # allow_headers=["*"],
+# )
 
 # 各因子题号（从1开始计数）
 FACTORS = {
